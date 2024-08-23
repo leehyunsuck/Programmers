b888fff5d2bb00a0ae4f76577f887c005e1bf07a
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-08-23 17:23:16 +0900

    [level 1] Title: 옹알이 (2), Time: 54.54 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,16 +3,13 @@ class Solution {
         int answer = 0;
         
         String[] poss = { "aya", "ye", "woo", "ma" };
-        String[] imposs = new String[poss.length];
-        for (int i = 0; i < poss.length; i++) imposs[i] = poss[i] + poss[i];
         
         for (String word : babbling) {
-            String temp = word;
-            for (String no : imposs) temp = temp.replaceAll(no, "");
-            if (temp.length() == word.length()) {
-                for (String yes : poss) word = word.replaceAll(yes, " ");
-                answer += word.isBlank() ? 1 : 0;
+            for (String p : poss) {
+                if (word.contains(p+p)) continue;
+                word = word.replaceAll(p, " ");
             }
+            answer += word.isBlank() ? 1 : 0;
         }
 
         return answer;
