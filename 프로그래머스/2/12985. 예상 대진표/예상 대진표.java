9fd162452f1c2693833e2d35118babc97a0f7503
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-08 12:05:34 +0900

    [level 2] Title: 예상 대진표, Time: 14.72 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        //a 에 더 작은 수 넣기
+        int temp = a;
+        a = a > b ? b : a;
+        b = a == b ? temp : b;
+        
+        int round = 0;
+
+        for(; a != b; round++) {
+            a = a % 2 == 1 ? a / 2 + 1: a / 2;
+            b = b % 2 == 1 ? b / 2 + 1: b / 2;
+            System.out.println(round + ", a : " + a + " , b : " + b);
+        }
+
+        return round;
+    }
+}
+/*
+                1 2     3 4     5 6         7 8
+                
+2로 나누면       0 1     1 2     2 3          3 4
+                1 0     1 0     1 0         1 0
+
+
+*/
