7733bc805b6ecf1e944c2d188011bf85859feb66
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-13 19:18:11 +0900

    [level 2] Title: n^2 배열 자르기, Time: 3.82 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -2,22 +2,20 @@ class Solution {
     public int[] solution(int n, long left, long right) {
         int[] answer = new int[(int) (right - left + 1)];
         
+        // 종료 조건 없이 배열값 넘어가면 종료
         try {
-            boolean first = true;
-            int start = (int) (left % (long) n) + 1;
-            for (int i = (int) (left / (long) n) + 1, realIdx = 0; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (first) {
-                        j = start;
-                        first = false;
-                    }
-                    answer[realIdx++] = Math.max(i, j);
-                }
+            int startI = (int) (left / (long) n) + 1,
+                startJ = (int) (left % (long) n) + 1,
+                index = 0;
+            
+            for (int j = startJ; j <= n; j++, index++)
+                answer[index] = Math.max(startI, j);
+            
+            for (int i = startI + 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++, index++) 
+                    answer[index] = Math.max(i, j);
             }
-        } catch (Exception e) {
-            // 비교 연산 시간 줄이기
-        }
-        
+        } catch (Exception e) { }
         
         return answer;
     }
@@ -50,4 +48,21 @@ left % n
     0 ~ 3       7 % 4   = 3
                 8 % 4   = 0
 
+
+초기 코드
+        try {
+            boolean first = true;
+            int start = (int) (left % (long) n) + 1;
+            for (int i = (int) (left / (long) n) + 1, realIdx = 0; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (first) {
+                        j = start;
+                        first = false;
+                    }
+                    answer[realIdx++] = Math.max(i, j);
+                }
+            }
+        } catch (Exception e) {
+            // 비교 연산 시간 줄이기
+        }
 */
