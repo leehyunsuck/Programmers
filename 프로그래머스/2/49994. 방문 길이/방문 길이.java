5cbd74b4c51382f65598a2b429a659daec778a6a
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-18 21:45:20 +0900

    [level 2] Title: 방문 길이, Time: 47.30 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,47 +1,49 @@
 import java.util.*;
 
 class Solution {
+    
+    private int[] location = {0, 0};
+    
     public int solution(String dirs) {
-        int[] location = {0, 0};
+        Map<Character, Integer[]> moveMap = new HashMap<>() {{
+            put('U', new Integer[] {0, 1});
+            put('D', new Integer[] {0, -1});
+            put('R', new Integer[] {1, 0});
+            put('L', new Integer[] {-1, 0});
+        }};
+        
         Set<String> tempSet = new HashSet<>();
         
         for (char c : dirs.toCharArray()) {
+            Integer[] moveInfo = moveMap.get(c);
             int[] temp = Arrays.copyOf(location, location.length);
-            switch (c) {
-                case 'U':
-                    location[1]++;
-                    if (location[1] > 5) location[1]--;
-                    break;
-                case 'D':
-                    location[1]--;
-                    if (location[1] < -5) location[1]++;
-                    break;
-                case 'L':
-                    location[0]++;
-                    if (location[0] > 5) location[0]--;
-                    break;
-                case 'R':
-                    location[0]--;
-                    if (location[0] < -5) location[0]++;
-                    break;
-                default:
-                    break;
-            }
             
-            String add = "";
-            if (location[0] == temp[0] && location[1] == temp[1]) continue;
+            location[0] += moveInfo[0];
+            location[1] += moveInfo[1];
+            
+            if (this.mapOver(moveInfo)) continue;
+            
+            String add = null;
             if (location[0] != temp[0]) {
                 add = location[0] < temp[0] 
-                    ? "x" + location[0] + temp[0] + location[1] + temp[1]
-                    : "x" + temp[0] + location[0] + temp[1] + location[1];
+                    ? "" + location[0] + temp[0] + location[1] + temp[1]
+                    : "" + temp[0] + location[0] + temp[1] + location[1];
             } else {
                 add = location[1] < temp[1] 
-                    ? "x" + location[0] + temp[0] + location[1] + temp[1] 
-                    : "x" + temp[0] + location[0] + temp[1] + location[1];
+                    ? "" + location[0] + temp[0] + location[1] + temp[1] 
+                    : "" + temp[0] + location[0] + temp[1] + location[1];
             }
             tempSet.add(add);
         }
         
         return tempSet.size();
+    }
+    
+    // 넘어가서 제자리인 경우 true 반환
+    public boolean mapOver(Integer[] moveInfo) {
+        if (location[0] > 5 || location[0] < -5) location[0] -= moveInfo[0];
+        else if (location[1] > 5 || location[1] < -5) location[1] -= moveInfo[1];
+        else return false;
+        return true;
     }
 }
