7079fae0e0e3136b54cdaa405b7c196a1cdce213
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-19 22:03:10 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 18.93 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,3 +1,45 @@
+// 문제 풀이 후 다른사람이 Split으로 풀이한 것 보고 해당 방식으로 다시 풀이
+
+import java.util.*;
+
+class Solution {
+    private Map<Long, Boolean> decimalMap = new HashMap<>();
+    
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        String[] trans = Integer.toString(n, k).split("0");
+        
+        for (String s : trans) {
+            if (s.length() == 0) continue;
+            if (this.isDecimal(Long.parseLong(s))) answer++;
+        }
+
+        return answer;
+    }
+    
+    // 소수 판별
+    public boolean isDecimal(Long n) {
+        if (n < 2 && !decimalMap.containsKey(n)) decimalMap.put(n, false);
+        if (decimalMap.containsKey(n)) return decimalMap.get(n);
+
+        int count = 0;
+        for (long i = 1; i * i <= n && count <= 2; i++) {
+            if (n % i == 0) {
+                count++;
+                if (n / i != i) count++;
+            }
+        }
+        decimalMap.put(n, count == 2);
+        
+        return decimalMap.get(n);
+    }
+}
+
+
+/*
+기존 코드
+
 import java.util.*;
 
 class Solution {
@@ -45,3 +87,6 @@ class Solution {
         return count == 2;
     }
 }
+
+
+*/
