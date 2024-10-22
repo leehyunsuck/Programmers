350a9e5aeb7022e74d868ea1e52a5608ac96975e
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-23 01:53:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean solution(int x) {
-        int sum = 0,
-            copyX = x;
-        while (copyX > 0) {
-            sum += copyX % 10;
-            copyX /= 10;
+        int sum = 0;
+        for (char c : String.valueOf(x).toCharArray()) {
+            sum += c - '0';
         }
+        
         return x % sum == 0;
     }
 }
 
-// n의 자리수 합으로 n의 나머지가 0이면 true
+// 반복문으로 나눠서 더함 -> 메모리: 80.1 MB, 시간: 0.04 ms
