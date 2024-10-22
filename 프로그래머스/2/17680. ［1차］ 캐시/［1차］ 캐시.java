d3548a808a2f4ed065b9c64f77a7d8e0f742481e
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-23 00:42:32 +0900

    [level 2] Title: [1차] 캐시, Time: 39.26 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,24 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        Queue<String> cache = new LinkedList<>();
-        
+        // 캐쉬 사이즈 없으면 모든 요청 실행시간이 5임
         if (cacheSize == 0) 
             return 5 * cities.length;
+        
+        Queue<String> cache = new LinkedList<>();
         
         int answer = 0;
         
         for (String city : cities) {
-            city = city.toLowerCase();
+            city = city.toLowerCase(); 
             
+            // 캐쉬에 해당 값이 있음
             if (cache.size() > 0 && cache.contains(city)) {
                 answer++;
-                cache.remove(city);    // 다시 위에 넣을거임
+                cache.remove(city);    // 사용했으니 다시 위에 넣기 위해 제거
             } else {
                 answer += 5;
                 
-                if (cache.size() == cacheSize)
-                    cache.remove();
+                if (cache.size() == cacheSize) cache.remove();
             }
             cache.add(city);
         }
@@ -27,5 +28,3 @@ class Solution {
         return answer;
     }
 }
-
-// 캐시에 있으면 1초, 없으면 5초
