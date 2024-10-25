a9d6d456b6c885140d49a0c62a00386a0fcc95ea
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-25 23:04:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 10.96 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,23 +1,11 @@
 class Solution {
     public long solution(long n) {
         
-        for (long i = 1; i * i <= n; i++) {
-            if (i * i == n) {
+        for (long i = 1; i <= n; i++) {
+            if (i * i == n)
                 return (i + 1) * (i + 1);
-            }
         }
         
         return -1;
     }
 }
-
-/*
-
-        double nSqrt = Math.sqrt(n);
-        if (nSqrt == (long) nSqrt) {
-            return (long) Math.pow(nSqrt + 1, 2);
-        }
-        
-        return -1;
-
-*/
