caf7741daafb5a197f0a41e1e184009b75cfaf5c
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-28 16:27:52 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 1.71 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,40 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        int[] temp = new int[arr.length];
+        // 동적 배열 == 리스트 == 공간 크기를 처음에 지정하지 않아
+        List<Integer> answerList = new ArrayList<>();
         
-        int idx = 0;
-        for (int a : arr) {
-            if (a % divisor == 0) 
-                temp[idx++] = a;
-        }
+        // arr 배열 모든 값을 탐색해
+        for (int num : arr)         // 원하는 조건의 값은 리스트에 넣어
+            if (num % divisor == 0) answerList.add(num);
+        answerList.sort(Comparator.naturalOrder());
         
-        if (idx == 0)
-            return new int[] {-1};
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answer.length; i++) 
+            answer[i] = answerList.get(i);
         
-        int[] answer = new int[idx];
-        for (int i = 0; i < idx; i++)
-            answer[i] = temp[i];
-        
-        this.sort(answer);
-        
-        return answer;
-    }
-    
-    public int[] sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int smallIdx = i;
-            
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[smallIdx])
-                    smallIdx = j;
-            }
-            
-            if (smallIdx == i) continue;
-            int big = arr[i];
-            arr[i] = arr[smallIdx];
-            arr[smallIdx] = big;
-        }
-        
-        return arr;
+        return answer.length == 0 ? new int[] {-1} : answer;
     }
 }
