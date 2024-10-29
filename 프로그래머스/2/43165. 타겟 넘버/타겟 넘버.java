b2d3417af65a2e4eb4f51b28c974b51b5d741bd6
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-30 00:44:08 +0900

    [level 2] Title: 타겟 넘버, Time: 8.99 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,4 +1,66 @@
 class Solution {
+    private int[] arr;
+    private int target,
+                result;
+    
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+    
+    public int getArrLen() {
+        return this.arr.length;
+    }
+    
+    public int getArrValue(int idx) {
+        return this.arr[idx];
+    }
+    
+    public void setTarget(int target) {
+        this.target = target;
+    }
+    
+    public int getTarget() {
+        return this.target;
+    }
+    
+    public void setResult(int result) {
+        this.result = result;
+    }
+    
+    public int getResult() {
+        return this.result;
+    }
+    
+    public void addOneResult() {
+        this.result++;
+    }
+    
+    public int solution(int[] numbers, int target) {
+        this.setArr(numbers);
+        this.setTarget(target);
+        this.setResult(0);
+        
+        this.dfs(0, 0);
+        
+        return this.getResult();
+    }
+    
+    public void dfs(int idx, int sum) {
+        if (idx == this.getArrLen()) {
+            if (sum == this.getTarget())
+                this.addOneResult();
+            return;
+        }
+        
+        this.dfs(idx + 1, sum + getArrValue(idx));
+        this.dfs(idx + 1, sum - getArrValue(idx));
+    }
+}
+
+/*  
+메모리: 85.9 MB, 시간: 5.22 ms
+
+class Solution {
     public int solution(int[] numbers, int target) {
         return this.dfs(numbers, target, 0, 0);
     }
@@ -14,3 +76,5 @@ class Solution {
                 this.dfs(arr, target, idx + 1, sum - arr[idx]);
     }
 }
+
+*/
