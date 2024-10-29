d48d17e44735f447fcf261fe0a26d597375d1918
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-29 23:17:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int solution(int num) {
-        long numToLong = (long) num;
+    public int solution(long num) {
         int answer = 0;
         
-        for (; numToLong != 1 && answer < 500; answer++) {
-            if (numToLong % 2 == 0) numToLong /= 2;
-            else numToLong = numToLong * 3l + 1l;
+        while(num != 1 && answer < 500) {
+            if (num % 2 == 0) num /= 2;
+            else num = num * 3 + 1;
+            answer++;
         }
-        
-        return answer < 500 ? answer : -1;
+
+        return answer == 500 ? -1 : answer;
     }
 }
