f54b4df3a03fa71903e2881ac87bcc1629cd3109
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-30 17:08:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,12 +1,19 @@
 class Solution {
     public int solution(long num) {
-        return this.collatzLoop(num, 0);
+        return recursionCollatz(num, 0);
     }
     
-    public int collatzLoop(long num, int loop) {
-        if (num == 1 || loop == 500) 
-            return loop == 500 ? -1 : loop;
+    public int recursionCollatz(long num, int repeat) {
+        if (repeat >= 500)  return -1;      // 반복횟수가 500이면, -1 반환
+        if (num == 1)       return repeat;  // num == 1이면, 반복횟수(repeat) 반환
         
-        return this.collatzLoop(num % 2l == 0l ? num / 2l : num * 3l + 1l, loop + 1);
+        // num이 1도 아니고, repeat이 500 이상도 아니면 아래 구간
+        
+        // 조건 처리
+        if (num % 2 == 0)   num /= 2l;
+        else                num = num * 3l + 1l;
+        
+        // num 조건 처리 한 값과, 반복횟수에 1 추가한 값으로 해당 메소드 재호출
+        return this.recursionCollatz(num, repeat + 1); 
     }
 }
