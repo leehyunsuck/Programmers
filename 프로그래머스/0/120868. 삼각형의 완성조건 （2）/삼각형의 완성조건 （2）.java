c1595b79933dad48b7254f1950b0e1773407ffa3
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-10-31 23:59:20 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.10 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,33 +1,15 @@
 class Solution {
     public int solution(int[] sides) {
-        return Math.min(sides[0], sides[1]) + sides[0] + sides[1] - Math.max(sides[0], sides[1]) - 1;
+        int answer = 0,
+            arrMax = Math.max(sides[0], sides[1]),
+            arrMin = Math.min(sides[0], sides[1]);
+        
+        // 3 , 6 -> 6 < 3 + 4 5 6
+        for (int i = arrMax - arrMin + 1; i <= arrMax && arrMin + i > arrMax; i++)  answer++;
+        for (int i = arrMax + 1; i < arrMax + arrMin; i++)                          answer++;
+        
+        return answer;
     }
 }
 
-/*
-가장 긴 변의 길이 = 다른 두 변의 길이의 합보다 작아야함
-
-[3 , 6]
-
-6을 가장 긴 변이라 할 때:
- 
- 다른 변의 길이를 x 라 할 때
- 6 - 3 < x <= 6    4 5 6
-
-다른 변이 가장 길다 할 때:
- 6 < x < 6 + 3
- 
--------
-
-        // 11 - 7 = 4
-        // 4보다 크고 11보다 작거나 같은 수
-        // 11 - 4 = 7
-        
-                // int a = sides[0];
-        // int b = sides[1];
-        // int maxAB = Math.max(a, b);
-        // int minAB = Math.min(a, b);
-        
-        //maxAB - (maxAB - minAB) + (a + b) - maxAB - 1;
-        // minAB + a + b - maxAB - 1
-*/
+// maxLen < aLen + bLen
