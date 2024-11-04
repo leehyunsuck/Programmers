e2f33b16af059b99e60e3a7cb2fc3debc2aaf889
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-11-04 12:23:47 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.93 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,27 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr) {
-        // 값 1개만 있으면 -1 반환
-        if (arr.length == 1)
-            return new int[] {-1};
-        
-        // 최소값 인덱스번호 탐색
         int minIdx = 0;
-        for (int idx = 0; idx < arr.length; idx++)
-            if (arr[idx] < arr[minIdx]) minIdx = idx;
         
-        // 최소값 인덱스 외 전부 복사
+        for (int idx = 0; idx < arr.length; idx++) {
+            if (arr[minIdx] > arr[idx]) minIdx = idx;
+        }
+        
         int[] answer = new int[arr.length - 1];
-        for (int idx = 0, answerIdx = 0; idx < arr.length; idx++, answerIdx++) {
-            if (idx == minIdx) {
-                answerIdx--;
-                continue;
-            }
-            answer[answerIdx] = arr[idx];
+        if (answer.length == 0) return new int[] {-1};
+        
+        int answerIdx = 0;
+        for (int arrIdx = 0; arrIdx < arr.length; arrIdx++) {
+            if (minIdx == arrIdx) continue;
+            answer[answerIdx++] = arr[arrIdx];
         }
         
         return answer;
     }
 }
-
-// 방법1. 정렬 후 1번 인덱스부터 복사
-// 방법2. 한번 쭉 탐색하고, 가장 작은 인덱스값 제외하고 복사
