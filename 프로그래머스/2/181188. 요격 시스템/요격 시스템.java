149b8d5a87595446a685b377c5dc4d7431861554
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-11-30 14:37:21 +0900

    [level 2] Title: 요격 시스템, Time: 387.36 ms, Memory: 190 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,7 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-        Arrays.sort(targets, (arr1, arr2) -> arr1[1] - arr2[1]);
+        Arrays.sort(targets, (arr1, arr2) -> {
+            int temp = arr1[1] - arr2[1];
+            if (temp != 0) return temp;
+            return arr1[0] - arr2[0];
+        });
 
         int answer = 0,
             end = 0;
