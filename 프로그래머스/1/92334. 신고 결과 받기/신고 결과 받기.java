3bbd4c81e11c4184883aaea33acf9b270477fd25
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-02 14:27:55 +0900

    [level 1] Title: 신고 결과 받기, Time: 162.12 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,83 +2,67 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] ids, String[] reports, int k) {
-        Map<String, Integer> answerMap = new HashMap<>();       // 유저 : 메일 받을 개수
-        Map<String, Set<String>> reportMap = new HashMap<>();   // 신고대상 : 신고자 (중복x)
+        // ids : 모든 유저의 이름
+        // reports : [누가 누구를] 신고여부
+        // k : 신고 몇번 당해야 정지냐
+        
+        /* ------------------------------------------------------------- */
+        
+        // 규칙1. 신고 제한 없음
+        // 규칙2. 동일한 유저에 대한 신고는 1회만 처리 됨
+        
+        /* ------------------------------------------------------------- */
+        
+        // 로직1. reports를 조회해서 특정유저를 누가 신고했는지 저장
+        //          -> 단 규칙2 지키면서
+        // 필요 자료구조  : Map -> String으로 조회해야함
+        //              : Set -> (규칙2) 동일한 신고자는 1회만 처리
 
-        // 두 맵 초기화
+        
+        // 로직2. 유저마다 정지 여부 판단
+        //          -> 정지다? 해당 유저를 신고한 사람들은 받을 메일 개수 증가
+        // 필요 자료구조  : Map -> 유저별로 몇개의 메일 받는지
+        
+        /* ------------------------------------------------------------- */
+        
+        // 필요한 자료구조 선언
+        // Key : 유저이름 , Value : 신고 한 사람 목록
+        Map<String, Set<String>> reportMap = new HashMap<>();
+        Map<String, Integer> getMailCountMap = new HashMap<>();
+        
+        // 선언한 자료구조 초기화
         for (String id : ids) {
-            answerMap.put(id, 0);
             reportMap.put(id, new HashSet<>());
+            getMailCountMap.put(id, 0);
         }
-
-        // reportMap 데이터 저장
+        
+        // [로직1] 수행
         for (String report : reports) {
-            // [0] -> 신고자, [1] -> 신고 대상
-            String[] reportSplit = report.split(" ");
-
-            Set<String> reportSet = reportMap.get(reportSplit[1]);
-            reportSet.add(reportSplit[0]);
-            reportMap.put(reportSplit[1], reportSet);
-        }
-
-        // 메일 받을 사람 확인
-        for (String id : ids) {
-            Set<String> reportSet = reportMap.get(id);
-
-            if (reportSet.size() < k) continue;
-            for (String report : reportSet)
-                answerMap.put(report, answerMap.get(report) + 1);
-        }
-
-        int[] answer = new int[answerMap.size()];
-        for (int i = 0; i < answer.length; i++)
-            answer[i] = answerMap.get(ids[i]);
-
-        return answer;
-    }
-}
-
-
-/*
- - User 1:N 신고 , But 동일인물은 1회만 판정
- - k 번 이상 신고된 유저 = 게시판 정지, 신고한사람한테 정지처분 메일 발송
- - 각 유저별 처리 결과를 메일로 받는 횟수
- 
- 3, 11, 14, 15, 20 ,21 시간초과
-import java.util.*;
-
-class Solution {
-    public int[] solution(String[] id_list, String[] report_list, int k) {
-        
-        //Map<유저 : 제재성공메일>
-        Map<String, Integer> resultMap = new HashMap<>();
-        
-        //Map 초기화
-        for (String id : id_list) resultMap.put(id, 0);
-
-        for (String id : id_list) {
-            //id를 신고한 유저 목록 담을 Set
-            Set<String> hashSet = new HashSet<>();
+            String[] reportInfo = report.split(" ");    //[누가 누구] 이렇게 들어가있으니 공백으로 나눔
+            String sendUser   = reportInfo[0],          // 누가 신고했는지
+                   targetUser = reportInfo[1];          // 누구를 신고했는지
             
-            for (String report : report_list) {
-                //[0]은 신고한 [1]은 신고당한
-                String[] reportLog = report.split(" ");
+            reportMap.get(targetUser).add(sendUser);    // 저장
+        }
+        
+        // [로직2] 수행
+        for (String userName : reportMap.keySet()) {
+            Set<String> usersWhoReported = reportMap.get(userName);
+            if (usersWhoReported.size() < k) continue;  // k 보다 작으면 정지 안당함
 
-                //set에 id를 신고한 사람 넣기
-                if (reportLog[1].equals(id)) hashSet.add(reportLog[0]);
+            // 정지임 -> 해당 유저를 신고한 사람들에게 메일 전송해야함
+            for (String user : usersWhoReported) {
+                getMailCountMap.put(user, getMailCountMap.get(user) + 1);
             }
-            
-            //id가 제재 대상이면
-            if (hashSet.size() >= k)
-                //id를 신고한사람 메일 카운트 증가
-                for (String rMU : hashSet) resultMap.put(rMU, resultMap.get(rMU) + 1);
         }
         
-        //결과값
-        int[] answer = new int[id_list.length];
-        for (int i = 0; i < id_list.length; i++) answer[i] = resultMap.get(id_list[i]);
+        int[] answer = new int[ids.length];
+        int idx = 0;
+        for (String id : ids) {
+            answer[idx++] = getMailCountMap.get(id);
+        }
 
+        // k번 이상 신고된 유저가 정지되었을 되었을 때 받을 메일의 개수
         return answer;
     }
 }
-*/
