023d9a46c7bdd7af4d51928efdf02b3423cae2f7
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-05 01:44:36 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 24.90 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,54 +1,39 @@
 import java.util.*;
 
 class Solution {
+    private int maxCount;
+    
     public String[] solution(String[] orders, int[] course) {
-        List<String> answerL = new ArrayList<>();
-
-        for (int len : course) {                                // 특정 개수 만큼 조합이 필요함
-            Map<String, Integer> orderCount = new HashMap<>();  // 메뉴조합 : 주문개수
-            for (String order : orders) {                       // 기존 주문 기록을 토대로 조회
+        Set<String> newMenus = new TreeSet<>();
+        
+        for (int len : course) {
+            this.maxCount = 0;
+            Map<String, Integer> countCheck = new HashMap<>();
+            for (String order : orders) {
                 char[] menus = order.toCharArray();
                 Arrays.sort(menus);
-                makeAndPut(orderCount, menus, "", len, 0);
+                makeNewMenu(countCheck, menus, len, "", 0); // 맵, 메뉴목록, 원하는길이, 세트메뉴명, 인덱스위치
             }
-            Set<String> mostOrderMenu = findMostOrder(orderCount, 2);   // 가장 많이 주문된 메뉴 조합 찾고
-            answerL.addAll(mostOrderMenu);                              // answer List에 저장
+            
+            if (this.maxCount < 2) continue;
+            for (String newMenu : countCheck.keySet()) {
+                if (countCheck.get(newMenu) == this.maxCount)
+                    newMenus.add(newMenu);
+            }
         }
-
-        String[] answer = answerL.toArray(new String[0]);
-        Arrays.sort(answer);
         
-        return answer;
+        return newMenus.toArray(String[]::new);
     }
     
-    // 조합 가능한 메뉴 찾기
-    // (메뉴조합 : 주문개수) , 메뉴목록, 새로운_메뉴_조합, 필요_메뉴_개수, 어디까지_메뉴는_사용했는지
-    public void makeAndPut(Map<String, Integer> map, char[] menus, String newMenu, int len, int minIdx) {
+    public void makeNewMenu(Map<String, Integer> countCheck, char[] menus, int len, String newMenu, int idx) {
         if (newMenu.length() == len) {
-            map.put(newMenu, map.getOrDefault(newMenu, 0) + 1);
+            int count = countCheck.getOrDefault(newMenu, 0) + 1;
+            countCheck.put(newMenu, count);
+            this.maxCount = Math.max(this.maxCount, count);
             return;
         }
         
-        for (int i = minIdx; i < menus.length; i++)
-            makeAndPut(map, menus, newMenu + menus[i], len, i + 1);
-    }
-    
-    // 가장 많이 주문된 메뉴 찾기
-    // (메뉴조합 : 주문개수) , 가장_많이_주문된_메뉴조합의_개수
-    public Set<String> findMostOrder(Map<String, Integer> map, int maxValue) {
-        Set<String> set = new HashSet<>();
-        
-        for (String key : map.keySet()) {
-            int value = map.get(key);
-            
-            if (value < maxValue) continue;
-            if (value > maxValue) {
-                set = new HashSet<>();
-                maxValue = value;
-            }
-            set.add(key);
-        }
-        
-        return set;
+        for (int i = idx; i < menus.length; i++) 
+            makeNewMenu(countCheck, menus, len, newMenu + menus[i], i + 1);
     }
 }
