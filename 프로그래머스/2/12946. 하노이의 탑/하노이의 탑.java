ba77b5bf2893ad9e39b219ba6819556095219337
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-06 01:50:12 +0900

    [level 2] Title: 하노이의 탑, Time: 2.21 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -3,21 +3,28 @@ import java.util.*;
 class Solution {
     List<int[]> answer;
     
-    public int[][] solution(int n) {
-        this.answer = new ArrayList<>();
-        hanoi(n, 1, 2, 3);
-        
-        return this.answer.toArray(int[][]::new);
+    private void reset() {
+        answer = new ArrayList<>();
     }
     
-    public void hanoi(int n, int from, int via, int  to) {
-        if (n == 1) {
-            this.answer.add(new int[] {from, to});
-            return;
-        }
-        
-        hanoi(n - 1, from, to, via);
-        this.answer.add(new int[] {from, to});
-        hanoi(n - 1, via, from, to);
+    public int[][] getAnswer() {
+        return answer.toArray(int[][]::new);
     }
+    
+    public void hanoi(int n, int from, int via, int to) {
+        if (n == 1) {
+            answer.add(new int[] {from, to});
+        } else {
+            hanoi(n - 1, from, to, via);
+            answer.add(new int[] {from, to});
+            hanoi(n - 1, via, from, to);
+        }    
+    }
+
+    public int[][] solution(int n) {
+        reset();
+        hanoi(n, 1, 2, 3);
+        return getAnswer();
+    }
+
 }
