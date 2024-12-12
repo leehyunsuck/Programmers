1d74c689ed6c89ea3b912ccecedc3a15225161b4
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-12 14:42:07 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 1.97 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -2,13 +2,6 @@ import java.math.*;
 
 class Solution {
     public long solution(int w, int h) {
-        BigInteger bigW = BigInteger.valueOf(w),
-                   bigH = BigInteger.valueOf(h);
-        
-        BigInteger add = bigW.add(bigH),
-                   multi = bigW.multiply(bigH),
-                   gcd = bigW.gcd(bigH);
-        
-        return multi.subtract(add.subtract(gcd)).longValue();
+        return ((long) w * h) - (w + h - BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue());
     }
 }
