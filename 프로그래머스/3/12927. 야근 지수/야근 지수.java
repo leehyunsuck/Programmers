ad0d5a919ac3a1acb5290ff7fc791814d488f4a1
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-19 01:33:02 +0900

    [level 3] Title: 야근 지수, Time: 125.03 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,33 +2,6 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        int big = 0;
-        
-        Map<Integer, Integer> workMap = new HashMap<>();
-        for (int work : works) {
-            workMap.put(work, workMap.getOrDefault(work, 0) + 1);
-            big = Math.max(big, work);
-        }
-        
-        for (; n > 0 && big > 0; n--) {
-            int count = workMap.get(big);
-            workMap.put(big, count - 1);
-            workMap.put(big-1, workMap.getOrDefault(big-1, 0) + 1);
-            
-            if (count - 1 == 0) big--;
-        }
-        
-        long answer = 0L;
-        for (Integer workTime : workMap.keySet()) {
-            for (int count = workMap.get(workTime); count > 0; count--)
-                answer += Math.pow(workTime, 2);
-        }
-        
-        return answer;
-    }
-}
-
-/*
         // 초기값 세팅
         Queue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
         for (int work : works) 
@@ -46,6 +19,11 @@ class Solution {
             answer += Math.pow(pQueue.poll(), 2);
         
         return answer;
+    }
+}
+
+/*
+        
 
 테스트 1 〉	통과 (0.99ms, 86.2MB)
 테스트 2 〉	통과 (0.50ms, 89MB)
