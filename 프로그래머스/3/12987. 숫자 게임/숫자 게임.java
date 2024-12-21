27181e093f24e3cd4cc252198bea90198260dc86
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-22 02:00:00 +0900

    [level 3] Title: 숫자 게임, Time: 134.89 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,61 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
+        Queue<Integer> aQueue = new PriorityQueue<>(Collections.reverseOrder()),
+                       bQueue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int idx = 0; idx < A.length; idx++) {
+            aQueue.offer(A[idx]);
+            bQueue.offer(B[idx]);
+        }
+        
         int answer = 0;
-        
-        Arrays.sort(A);
-        Arrays.sort(B);
-        
-        int bIdx = 0;
-        for (int aNum : A) {
-            while (bIdx < B.length && B[bIdx] <= aNum) bIdx++;
-            if (bIdx >= B.length) break;
-            answer++;
-            bIdx++;
+        while (!aQueue.isEmpty()) {
+            int aNum = aQueue.poll(),
+                bNum = bQueue.peek();
+            
+            if (bNum > aNum) {
+                answer++;
+                bQueue.poll();
+            }
         }
         
         return answer;
     }
 }
-
-// N명씩 두팀으로 나누기
-// A B 공수 , 큰 쪽이 1점 흭득
-// B팀이 이길 수 있는 방식으로 배치하여, 가장 많이 얻는 점수
-
-/*
-시간초과
-    public int solution(int[] A, int[] B) {
-        int answer = 0;
-        
-        // 숫자 : 개수
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int number : B) {
-            map.put(number, map.getOrDefault(number, 0) + 1);
-        }
-        
-        for (int aNum : A) {
-            int selectKey = 1_000_000_001,
-                minKey    = 1_000_000_001;
-            for (int key : map.keySet()) {
-                if (key > aNum) selectKey = Math.min(selectKey, key);
-                minKey = Math.min(minKey, key);
-            }
-            
-            if (selectKey == 1_000_000_001) {
-                valueSubOne(map, minKey);
-                continue;
-            }
-            
-            answer++;
-            valueSubOne(map, selectKey);
-        }
-        
-        return answer;
-    }
-    
-    public void valueSubOne(Map<Integer, Integer> map, int key) {
-        int numSubOne = map.get(key) - 1;
-        if (numSubOne > 0) map.put(key, numSubOne);
-        else map.remove(key);
-    }
-*/
