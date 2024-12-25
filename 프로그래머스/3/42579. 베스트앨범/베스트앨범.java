1efc95f645ffbd0cfb36570bffc88e2b8da6ac89
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-25 21:41:03 +0900

    [level 3] Title: 베스트앨범, Time: 5.05 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,60 +1,84 @@
 import java.util.*;
 
 class Solution {
+    class Unit {
+        private int idx;
+        private int value;
+        
+        public Unit(int idx, int value) {
+            this.idx = idx;
+            this.value = value;
+        }
+        
+        public int getIdx() {
+            return idx;
+        }
+        
+        public int getValue() {
+            return value;
+        }
+    }
+    
     public int[] solution(String[] genres, int[] plays) {
-        // 노래장르 : 내림차순 재생횟수 (가장 큰 값은 전체 합)
-        Map<String, List<int[]>> infoMap = new HashMap<>();
-        Map<String, Integer> totalMap = new HashMap<>();
-        
-        for (int idx = 0; idx < genres.length; idx++) {
-            String key = genres[idx];
-            int[] value = new int[] {plays[idx], idx};
-            
-            List<int[]> list = infoMap.getOrDefault(key, new ArrayList<>());
-            list.add(value);
-            infoMap.put(key, list);
-            
-            totalMap.put(key, totalMap.getOrDefault(key, 0) + value[0]);
-        }
-        
-        // 큰 값부터 정렬하는 용도
-        List<Map.Entry<String, Integer>> entry = new ArrayList<>(totalMap.entrySet());
-        entry.sort( (e1, e2) -> e2.getValue().compareTo(e1.getValue()));
-        
-        // 큰 값부터 노래장르를 리스트에 넣음
-        // 추가로 [노래재생횟수, 인덱스] 를 노래재생횟수가 큰 값부터, 같다면 인덱스가 작은 값부터 정렬
-        List<String> keys = new ArrayList<>();
-        for (Map.Entry<String, Integer> e : entry) {
-            keys.add(e.getKey());
-            
-            List<int[]> list = infoMap.get(e.getKey());
-            list.sort( (a1, a2) -> {
-                int a1Count = a1[0],
-                    a2Count = a2[0],
-                    a1Idx = a1[1],
-                    a2Idx = a2[1];
-                
-                int compare = a2Count - a1Count;
-                if (compare != 0) return compare;
-                return a1Idx - a2Idx;
-            });
-        }
-        
-        List<Integer> answerList = new ArrayList<>();
-        for (String key : keys) {
-            List<int[]> list = infoMap.get(key);
-            
-            for (int i = 0; i < Math.min(list.size(), 2); i++) {
-                answerList.add(list.get(i)[1]);
-            }
-        }
-        
-        int[] answer = new int[answerList.size()];
-        for (int idx = 0; idx < answer.length; idx++) {
-            answer[idx] = answerList.get(idx);
-        }
+        Map<String, Integer> genreAllPlayCount = new HashMap<>();
+        Map<String, List<Unit>> unitMap = new HashMap<>();
+
+        resetFieldMap(genres, plays, genreAllPlayCount, unitMap);
+        sortUnitMap(unitMap);
+
+        int[] answer = getResult(genreAllPlayCount, unitMap);
         
         return answer;
+    }
+
+    public int[] getResult(Map<String, Integer> gMap, Map<String, List<Unit>> uMap) {
+        List<Integer> resultList = new ArrayList<>();
+
+        for (String key : getSortedKeyList(gMap)) {
+            List<Unit> unitList = uMap.get(key);
+
+            for (int i = 0; i < Math.min(unitList.size(), 2); i++) {
+                resultList.add(unitList.get(i).getIdx());
+            }
+        }
+
+        return resultList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public void sortUnitMap(Map<String, List<Unit>> map) {
+        for (String key : map.keySet()) {
+            List<Unit> list = map.get(key);
+            list.sort( (u1, u2) -> {
+                int compare = u2.getValue() - u1.getValue();
+                if (compare != 0) return compare;
+                return u1.getIdx() - u2.getIdx();
+            });
+        }
+    }
+
+    public List<String> getSortedKeyList(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort( (e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : entryList) {
+            result.add(entry.getKey());
+        }
+
+        return result;
+    }
+
+    public void resetFieldMap(String[] genres, int[] plays, Map<String, Integer> genreAllPlayCount, Map<String, List<Unit>> unitMap) {
+        for (int idx = 0; idx < genres.length; idx++) {
+            String key = genres[idx];
+            int count = plays[idx];
+
+            List<Unit> list = unitMap.getOrDefault(key, new ArrayList<>());
+            list.add(new Unit(idx, count));
+            unitMap.put(key, list);
+
+            genreAllPlayCount.put(key, genreAllPlayCount.getOrDefault(key, 0) + count);
+        }
     }
 }
 
