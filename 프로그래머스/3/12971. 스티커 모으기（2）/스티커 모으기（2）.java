117fbe994dc15dad9a65a1ca4062f68b4d5164c5
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2024-12-28 15:12:48 +0900

    [level 3] Title: 스티커 모으기(2), Time: 8.34 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -3,23 +3,20 @@ class Solution {
         if (sticker.length == 1)
             return sticker[0];
         
-        return Math.max(dynamicProgramming(sticker, 2), dynamicProgramming(sticker, 1));
+        return Math.max(dynamicProgramming(sticker, true), dynamicProgramming(sticker, false));
     }
     
-    public int dynamicProgramming(int[] sticker, int option) {
+    public int dynamicProgramming(int[] sticker, boolean first) {
         int[] dp = new int[sticker.length];
         
-        if (option == 2) {
-            dp[0] = sticker[0];
-            dp[1] = dp[0];
-        } else {
-            dp[0] = 0;
-            dp[1] = sticker[1];
-        }
-        for (int i = 2; i < sticker.length - option + 1; i++)
+        dp[0] = first ? sticker[0] : 0;
+        dp[1] = first ? dp[0] : sticker[1];
+ 
+        int option = first ? 1 : 0;
+        for (int i = 2; i < sticker.length - option; i++)
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + sticker[i]);
         
         
-        return dp[sticker.length - option];
+        return dp[sticker.length - 1 - option];
     }
 } 
