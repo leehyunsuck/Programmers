3b1f7330cccb52823e2a5e4e11e038343e7e6465
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2025-01-23 07:47:53 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.91 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,50 +1,41 @@
 import java.util.*;
 
-// 아 뭔 좌측 상단이 1,1이야
-// 제대로 안읽은 잘못 시간으로 벌받았다
-
 class Solution {
-    // 사전순 순서로 방향 설정: 'd', 'l', 'r', 'u'
-    private static final int[][] DIRS = {
-        {1, 0, 'd'}, {0, -1, 'l'}, {0, 1, 'r'}, {-1, 0, 'u'}
-    };
+    private static final int[][] DIRS = {{'d', 1, 0}, {'l', 0, -1}, {'r', 0, 1}, {'u', -1, 0}};
     
     private String answer = "impossible";
     private int n, m, r, c, k;
     
-    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         this.n = n; this.m = m; this.r = r; this.c = c; this.k = k;
         
-        dfs(x, y, new StringBuilder());
-        
+        dfs(new StringBuilder(), x, y);
         return answer;
     }
     
-    private void dfs(int row, int col, StringBuilder builder) {
-        if (!answer.equals("impossible")) return;   // 최초 발견이 사전순 첫번째임
+    private void dfs(StringBuilder builder, int row, int col) {
+        if (row < 1 || col < 1) return;
+        if (row > n || col > m) return;
+        if (!answer.equals("impossible")) return;
+        if (!canGetToExit(builder, row, col)) return;
         
         if (builder.length() == k) {
             if (row == r && col == c) answer = builder.toString();
             return;
         }
         
-        int canMoveCount  = k - builder.length(),                   // 움직일 수 있는 횟수
-            needMoveCount = Math.abs(r - row) + Math.abs(c - col);  // 필요한 횟수
-        
-        if (canMoveCount < needMoveCount) return;                   // 걍 물리적으로 갈수 없음
-        if ((canMoveCount - needMoveCount) % 2 == 1) return;        // 상하좌우 밖에 없으니 1칸 남으면 못감
-        
-        for (int[] moveInfo : DIRS) {
-            int nextRow = row + moveInfo[0],
-                nextCol = col + moveInfo[1];
-            
-            if (nextRow < 1 || nextCol < 1) continue;
-            if (nextRow > n || nextCol > m) continue;
-            
-            builder.append((char) moveInfo[2]);
-            dfs(nextRow, nextCol, builder);
+        for (int[] dir : DIRS) {
+            builder.append((char) dir[0]);
+            dfs(builder, row + dir[1], col + dir[2]);
             builder.deleteCharAt(builder.length() - 1);
-        }
+        }    
+    }
+    
+    private boolean canGetToExit(StringBuilder builder, int row, int col) {
+        int canMoveCount = k - builder.length();
+        int needMoveCount = Math.abs(r - row) + Math.abs(c - col);
+        
+        if (canMoveCount < needMoveCount) return false;
+        return (canMoveCount - needMoveCount) % 2 == 0;
     }
 }
