b6ae110e28f32de4390cd14505f93e9908ea68b2
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2025-01-25 15:01:04 +0900

    [level 3] Title: 양과 늑대, Time: 4.79 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,24 +1,41 @@
+// 채점 후 가독성 향상 연습
+
 import java.util.*;
 
 class Solution {
     private class Info {
+        // 값 사용 여부
         boolean[] used;
-        int wolf, sheap, nowIdx;
+        // 카운팅 및 현재 위치
+        int wolf, sheep, nowIdx; 
         
-        Info(boolean[] used, int wolf, int sheap, int nowIdx) {
+        // 루트 생성자
+        Info(boolean[] used) {
             this.used = used;
-            this.wolf = wolf;
-            this.sheap = sheap;
+            this.wolf = this.sheep = this.nowIdx = 0;
+        }
+        
+        // 다음 정보 생성자
+        Info(Info info, int nowIdx) {
+            this.used = info.used.clone();
+            this.wolf = info.wolf;
+            this.sheep = info.sheep;
             this.nowIdx = nowIdx;
         }
         
-        Info copy() {
-            return new Info(this.used.clone(), this.wolf, this.sheap, this.nowIdx);
+        void use(int[] info) {
+            if (used[nowIdx]) return;
+            if (info[nowIdx] == 0) sheep++;
+            else wolf++;
+            used[nowIdx] = true;
         }
         
-        String getString() {
+        // 왜 String.format이나 '+' 연산으로 안하냐는 질문 금지
+        // 이게 더 빨라
+        @Override
+        public String toString() {
             StringBuilder builder = new StringBuilder();
-            builder.append(nowIdx).append(",").append(wolf).append(",").append(sheap).append(",");
+            builder.append(nowIdx).append(",").append(wolf).append(",").append(sheep).append(",");
             for (boolean b : used)
                 builder.append(b ? "1" : "0");
             return builder.toString();
@@ -26,66 +43,52 @@ class Solution {
     }
     
     public int solution(int[] info, int[][] edges) {
-        int answer = 0;
+        List<List<Integer>> graph = createGraph(edges, info.length);
         
-        int len = info.length;
-        
-        List<List<Integer>> graph = createGraph(edges, len);
-        
-        Queue<Info> queue = new LinkedList<>();
-        queue.offer(new Info(new boolean[len], 0, 0, 0));
-        
-        Set<String> visited = new HashSet<>();
-        
-        while (!queue.isEmpty()) {
-            Info poll = queue.poll();
-            
-            if (!poll.used[poll.nowIdx]) {
-                if (info[poll.nowIdx] == 0) poll.sheap++;
-                else poll.wolf++;
-                poll.used[poll.nowIdx] = true;
-            }
-            
-            if (poll.wolf >= poll.sheap) continue;
-            if (visited.contains(poll.getString())) continue;
-            
-            visited.add(poll.getString());
-            answer = Math.max(answer, poll.sheap);
- 
-            for (int idx : graph.get(poll.nowIdx)) {
-                Info copy = poll.copy();
-                copy.nowIdx = idx;
-                queue.offer(copy);
-            }
-        }
-        
-        return answer;
+        return getMaxSheapCount(graph, info);
     }
     
+    // 그래프 탐색
+    private int getMaxSheapCount(List<List<Integer>> graph, int[] info) {
+        int result = 0;
+        
+        Queue<Info> queue = new LinkedList<>();
+        queue.offer(new Info(new boolean[info.length]));
+        
+        // 동일한 조건으로 방문했는지 판단하기 위해 사용
+        Set<String> visited = new HashSet<>();
+        while (!queue.isEmpty()) {
+            Info poll = queue.poll();
+            poll.use(info);
+
+            if (!validateRuleAndVisited(poll, visited)) continue;
+            visited.add(poll.toString());
+            
+            result = Math.max(result, poll.sheep);
+            
+            for (int idx : graph.get(poll.nowIdx))
+                queue.offer(new Info(poll, idx));
+        }
+        
+        return result;
+    }
+    
+    private boolean validateRuleAndVisited(Info info, Set<String> visited) {
+        return info.wolf < info.sheep && !visited.contains(info.toString());
+    }
+    
+    // 그래프 생성
     private List<List<Integer>> createGraph(int[][] edges, int len) {
         List<List<Integer>> graph = new ArrayList<>();
         
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) 
             graph.add(new ArrayList<>());
-        }
         
-        for (int i = 0; i < edges.length; i++) {
-            int parent = edges[i][0],
-                child  = edges[i][1];
-            
-            graph.get(parent).add(child);
-            graph.get(child).add(parent);
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
-        
+
         return graph;
     }
 }
-
-// 양을 모아
-// 방문하면 해당 노드의 동물은 나를 따라와
-// 늑대가 양보다 많으면 잡아먹혀
-
-// 왔던칸 지나갈수는 있음
-
-// info[] 0 == 양, 1 == 늑대
-// edges[][] 연결정보
