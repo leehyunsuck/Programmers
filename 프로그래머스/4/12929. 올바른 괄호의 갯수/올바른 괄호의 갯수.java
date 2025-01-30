bd50e0456b9ccce8c6dbb9e0944aff41bfc39205
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2025-01-31 05:59:22 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 155.88 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        if (n == 1) return 1;
-        
-        return dfs(0, 0, n * 2);
+        return n == 1 ? 1 : dfs(0, 0, n * 2);
     }
     
     private int dfs(int open, int close, int max) {
-        if (close > open) return 0;
-        
-        if (open + close == max) 
-            return open == close ? 1 : 0;
-        
+        if (close > open)        return 0;
+        if (open + close == max) return open == close ? 1 : 0;
         return dfs(open + 1, close, max) + dfs(open, close + 1, max);
     }
 }
-
-// BFS는 시간초과
