fdeb6f793653afe2284c22330c9089502abcd02e
hyunsuck <144207343+leehyunsuck@users.noreply.github.com>
2025-02-01 13:57:51 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 21.63 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -2,53 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] lines) {
-        int len = lines.length;
-        int[][] times = new int[len][2];
-
-        for (int i = 0; i < len; i++) {
-            String[] log = lines[i].split(" ");
-            int endTime   = toMiliSec(log[1]),
-                needTime  = toMiliSec(log[2].replace("s", "")),
-                startTime = endTime - needTime + 1;
-            times[i][0] = startTime;
-            times[i][1] = endTime;
+        int[][] logs = new int[lines.length][2];
+        
+        for (int idx = 0; idx < logs.length; idx++) {
+            String[] log = lines[idx].split(" ");
+            logs[idx][1] = toMiliSecond(log[1]);
+            logs[idx][0] = logs[idx][1] - toMiliSecond(log[2].replace("s", "")) + 1;
         }
-
-        int answer = 0;
-        for (int i = 0; i < len; i++) {
-            int endTime  = times[i][1],
-                lastTime = endTime + 999;   // endTime부터 1초 판단이니까 999 더한 시간까지만
-            
-            int count = 0;
-            for (int j = 0; j < len; j++) {
-                if (times[j][1] < endTime) continue;
-                if (times[j][0] > lastTime) continue;
+        
+        return getMaxProcessOneSecond(logs);
+    }
+    
+    // 1초동안 가장 많이 처리한 작업 개수
+    private int getMaxProcessOneSecond(int[][] logs) {
+        int result = 0;
+        for (int i = 0, count = 0; i < logs.length; i++, count = 0) {
+            for (int j = i, oneSecStart = logs[i][1], oneSecEnd   = oneSecStart + 999; j < logs.length; j++) {
+                if (logs[j][0] > oneSecEnd ||
+                    logs[j][1] < oneSecStart) continue;
                 count++;
             }
-            
-            answer = Math.max(answer, count);
+            if (result < count) result = count;
         }
-
-        return answer;
+        
+        return result;
     }
-
-    private int toMiliSec(String time) {
-        if (time.length() < 12) {
-            if (time.length() == 1) return Integer.parseInt(time) * 1000;
+    
+    // "HH:MM:SS.SSS", "S.SSS" 형태의 문자열을 밀리초 단위의 정수로 반환
+    private int toMiliSecond(String time) {
+        if (time.length() <= 5) {
+            if (time.length() == 1) return Integer.parseInt(time) * 1_000;
+            
             String[] split = time.split("\\.");
-            return Integer.parseInt(split[0]) * 1000 + (split.length > 1 ? Integer.parseInt(split[1]) : 0);
+            while (split[1].charAt(0) == 0)
+                split[1] = split[1].substring(1) + "0";
+            
+            return Integer.parseInt(split[0]) * 1_000 + Integer.parseInt(split[1]);
         }
-        int hour = Integer.parseInt(time.substring(0, 2));
-        int min  = Integer.parseInt(time.substring(3, 5));
-        int sec  = Integer.parseInt(time.substring(6, 8));
-        int mils = (time.length() > 9) ? Integer.parseInt(time.substring(9)) : 0;
-
-        min += hour * 60;
-        sec += min * 60;
-        mils += sec * 1000;
-
-        return mils;
+        int hour = Integer.parseInt(time.substring(0, 2)), min     = Integer.parseInt(time.substring(3, 5)),
+            sec  = Integer.parseInt(time.substring(6, 8)), miliSec = Integer.parseInt(time.substring(9));
+        
+        return (hour * 3600 + min * 60 + sec) * 1000 + miliSec;
     }
 }
-
-// 너무 어려웠다
